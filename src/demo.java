0e7d45bb363978fb02d16167d4a98ff22e90a243
tongqinghua <qinghuatongxue@outlook.com>
2024-02-06 13:28:06 +0800

    test

---
+++ src/demo.java
@@ -0,0 +1,10 @@
+/**
+ * @author : Tong
+ * @Description: TODO
+ * @date Date : 2024-02-06
+ */
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello word");
+    }
+}
