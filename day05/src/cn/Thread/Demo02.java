701eb14438a030e6ee63bc18a9dabd0d2a25a9cd
tongqinghua <qinghuatongxue@outlook.com>
2024-05-19 14:29:50 +0800

    day05

---
+++ day05/src/cn/Thread/Demo02.java
@@ -0,0 +1,12 @@
+package cn.Thread;
+
+/**
+ * @author : Tongqh
+ * @Description: TODO
+ * @date Date : 2024-05-19
+ */
+public class Demo02 {
+    public static void main(String[] args) {
+        System.out.println("这是一个测试");
+    }
+}
